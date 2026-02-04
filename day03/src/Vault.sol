a801c0a5166bbb8c616f3a1dec47823718cbf305
diego <dmacia203@gmail.com>
2026-02-04 15:50:49 +0100

    task02

--- day03/src/Vault.sol
+++ day03/src/Vault.sol
@@ -2,11 +2,25 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Vault {
+// Custom Errors
+error ZeroAmount();
+error InsufficientShares();
+error ZeroShares();
+
+contract Vault is ReentrancyGuard, Ownable {
+    using SafeERC20 for IERC20;
+
     IERC20 public immutable asset;
     uint256 public totalShares;
     mapping(address => uint256) public sharesOf;
+
+    // Events
+    event Deposit(address indexed user, uint256 assets, uint256 shares);
+    event Withdraw(address indexed user, uint256 assets, uint256 shares);
 
     constructor(address _asset) {
         asset = IERC20(_asset);
@@ -36,5 +50,50 @@ contract Vault {
         // Formula: assets = (shares × totalAssets) / totalShares
         // Multiply before dividing to preserve precision
         return (shares * totalAssets) / totalShares;
+    }
+
+    function deposit(uint256 assets) external nonReentrant returns (uint256 shares) {
+        // C - CHECKS
+        if (assets == 0) revert ZeroAmount();
+        
+        shares = _convertToShares(assets);
+        if (shares == 0) revert ZeroShares();
+
+        // E - EFFECTS
+        sharesOf[msg.sender] += shares;
+        totalShares += shares;
+
+        // I - INTERACTIONS
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        emit Deposit(msg.sender, assets, shares);
+    }
+
+    function withdraw(uint256 shares) public nonReentrant returns (uint256 assets) {
+        // C - CHECKS
+        if (shares == 0) revert ZeroAmount();
+        if (sharesOf[msg.sender] < shares) revert InsufficientShares();
+
+        // E - EFFECTS
+        assets = _convertToAssets(shares);
+        sharesOf[msg.sender] -= shares;
+        totalShares -= shares;
+
+        // I - INTERACTIONS
+        asset.safeTransfer(msg.sender, assets);
+
+        emit Withdraw(msg.sender, assets, shares);
+    }
+
+    function withdrawAll() public returns (uint256 assets) {
+        assets = withdraw(sharesOf[msg.sender]);
+    }
+
+    function previewDeposit(uint256 assets) external view returns (uint256) {
+        return _convertToShares(assets);
+    }
+
+    function previewWithdraw(uint256 shares) external view returns (uint256) {
+        return _convertToAssets(shares);
     }
 }
